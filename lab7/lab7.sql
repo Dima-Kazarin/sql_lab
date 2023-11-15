b777a7c8173117733fdebc20a04a524b9151949b
Dima <dimakazarin66@gmail.com>
2023-11-15 09:01:11 +0200

    lab7 (change)

--- lab7/lab7.sql
+++ lab7/lab7.sql
@@ -21,7 +21,7 @@ CREATE TABLE `comments` (
   `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
   `news_id` int(10) UNSIGNED NOT NULL,
   `user_id` int(10) UNSIGNED,
-  `text` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `text` text COLLATE utf8_unicode_ci NOT NULL,
   `creation_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
